b5a27628ee110cedede927a9e2cc10d21774d883
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-03 19:24:39 +0100

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <script>
+// Import axios
+import axios from 'axios';
+// Endpoint
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
+
+// Import Store precedentemente esportato da store.js
+import { store } from './data/store.js'
+
+// Import AppMain
 import AppMain from './/components/AppMain.vue';
 
-export default {
-    data() {
-        return {
 
-        }
-    },
-    components: { AppMain }
-}
+export default {
+    components: { AppMain, },
+
+    // Alla chiamata Created prendiamo i dati e li pasiamo al file STORE.js
+    created() {
+        axios.get(endpoint).then(res => {
+            store.characters = res.data.docs;
+        });
+    }
+};
 </script>
 
 <template>
