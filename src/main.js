3797b502bafec0def5f56408fbae0eb8729dbf43
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-03 14:31:26 +0100

    bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+
+// import font "montserrat"
 import '@fontsource/montserrat'
+
+// import bootstrap min css
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
