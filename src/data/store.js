dc284ee23af8361d6b569ad6aeec7873f0fac9f1
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-03 19:23:21 +0100

    data-store.js

---
+++ src/data/store.js
@@ -0,0 +1,8 @@
+// Funzione Reactive di Vue per creare un oggetto che si aggiorna 
+import { reactive } from "vue";
+
+// Esporto la variabile Store con funzione Reactive
+export const store = reactive({
+    // con oggettto  characters vuoto
+    characters: []
+});
